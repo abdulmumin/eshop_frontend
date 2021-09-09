75e29df9e4eecda6132050dee47a17a588a28035
abdulmumin <abdulmuminabdulhamid025@gmail.com>
2021-09-09 11:49:01 -0700

    text

---
+++ src/ProductScreen.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+import ProductsCard from './ProductCards';
+import { useDispatch, useSelector, } from 'react-redux';
+import LoadingBox from './LoadingBox';
+import MessageBox from './MessageBox';
+import { listProduct } from './ReduxStore/actions/productAction';
+
+const HomeScreen = () => {
+    const dispatch = useDispatch()
+    const productList  = useSelector(state => state.productList )
+    const {products, loading, error } = productList;
+    useEffect(() => {
+       dispatch(listProduct())
+    }, [dispatch]);
+    return (
+        <div>
+            {
+                loading ? <LoadingBox></LoadingBox>
+                :error ? <MessageBox variant="danger">{}error</MessageBox>
+                :<div className="row and center">
+                {
+                    products.map(products => (
+                        <ProductsCard key={products._id} products={products} alt={products.name} />
+                    ))
+                }
+                
+            </div>
+            }
+        </div>
+    )
+}
+export default HomeScreen
